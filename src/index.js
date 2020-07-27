9431a97f2c8f68348f47c0754ada5e381cc8054e
Freet8707 <freet8707@gmail.com>
2020-07-26 22:16:44 -0500

    initial framework for redux pattern initialized

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const reducer = state => {
+    return {}
+}
+
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>,
+    rootElement
+);
